28437c368a044ae2245327fe2d342eddf76b51a6
Danang Adi Nugroho <danangtar@gmail.com>
2022-06-12 22:26:07 +0700

    add promiseOutput

--- promise.js
+++ promise.js
@@ -1,7 +1,26 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise((r) => {
+    promiseTheaterIXX().then((dataIXX) => {
+      var countIXX = countEmo(dataIXX, emosi);
+      promiseTheaterVGC().then((dataVGC) => {
+        var countVGC = countEmo(dataVGC, emosi);
+        r(countIXX + countVGC);
+      })
+    });
+  });
+};
+
+const countEmo = (data, emosi) => {
+  var count = 0;
+  data.forEach(film => {
+    if(film.hasil == emosi)
+      count++;
+  });
+  return count;
+}
 
 module.exports = {
   promiseOutput,
